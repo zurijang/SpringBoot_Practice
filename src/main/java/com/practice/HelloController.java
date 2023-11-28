a7cb620912c3c41550792b5c1d631354d10ebf79
zurijang <wkdwn05@gmail.com>
2023-11-28 15:26:13 +0900

    refactor : separate service logic in controller class to service class

--- src/main/java/com/practice/HelloController.java
+++ src/main/java/com/practice/HelloController.java
@@ -1,10 +1,16 @@
 package com.practice;
 
+import java.util.Objects;
+
 public class HelloController {
 
 	public String hello(String name) {
 		
-		return "Hello " + name;
+		SimpleHelloService helloService = new SimpleHelloService();
+		
+		// 파라미터 name 겁증 후 비즈니스 로직에 전달
+		// Objects.requireNonNull Object를 넘겨두고 만약에 Null이라면 예외를 던지고 아니면 이 값을 그대로 리턴해줌 (Null Check)
+		return helloService.sayHello(Objects.requireNonNull(name));
 		
 	}
 }
---
+++ src/main/java/com/practice/SimpleHelloService.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class SimpleHelloService {
+
+	// name 파라미터를 활용해서 문자열을 반환하는 서비스 로직
+	String sayHello(String name) {
+		
+		return "Hello " + name;
+		
+	}
+	
+}
