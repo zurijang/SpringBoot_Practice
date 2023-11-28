5bf19254b21c077e5309ac48a0491efaa525407c
zurijang <wkdwn05@gmail.com>
2023-11-28 14:07:00 +0900

    feat : convert servlet container to front controller

--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -26,22 +27,42 @@ public class PracticeApplication {
 			@Override
 			public void onStartup(ServletContext servletContext) throws ServletException {
 				
-				servletContext.addServlet("hello", new HttpServlet() {
+				servletContext.addServlet("FrontController", new HttpServlet() {
 
 					@Override
 					protected void service(HttpServletRequest req, HttpServletResponse resp)
 							throws ServletException, IOException {
-
-						// 응답코드 설정
-						resp.setStatus(HttpStatus.OK.value());
-						// 헤더 설정
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						// 바디 설정
-						resp.getWriter().println("Hello Servlet");
+						
+						// 인증, 보안, 다국어, 공통 기능 (서비스 로직 전처리)
+						/* ... */
+						
+						// GET Method로 들어오는 /hello 요청에 대해서만 처리
+						if( req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name()) ) {
+						
+							String name = req.getParameter("name");
+							
+							// 응답코드 설정
+							resp.setStatus(HttpStatus.OK.value());
+							// 헤더 설정
+							resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+							// 바디 설정
+							resp.getWriter().println("Hello Servlet " + name);
+						
+						}
+						else if(req.getRequestURI().equals("/user")) {
+							
+							
+							
+						} else {
+							
+							// 의도하지 않은 요청으로 들어왔을 때 응답으로 BAD_REQUEST 리턴
+							resp.setStatus(HttpStatus.BAD_REQUEST.value());
+							
+						}
 						
 					}
 					
-				}).addMapping("/hello");
+				}).addMapping("/*");
 				
 			}
 			
