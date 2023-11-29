40a226a7cd2595264d4b2d998392fc71934b9ff7
zurijang <wkdwn05@gmail.com>
2023-11-29 09:13:38 +0900

    chore : ..

--- src/main/java/com/practice/HelloController.java
+++ src/main/java/com/practice/HelloController.java
@@ -2,8 +2,16 @@ package com.practice;
 
 import java.util.Objects;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+// Class Level에 /hello 요청 지정
+@RequestMapping("/hello")
+@Controller
+public class HelloController {
 	
 	private final HelloService helloService;
 	
@@ -11,7 +19,10 @@ public class HelloController {
 		this.helloService = helloService;
 	}
 	
-	public String hello(String name) {
+	// Class Level 의 /hello 요청 중 Get Method 만 처리 (따로 요청을 지정하지 않았을 경우)
+	@GetMapping
+	@ResponseBody
+	public String hello(@RequestParam("name") String name) {
 		
 		// 파라미터 name 겁증 후 비즈니스 로직에 전달
 		// Objects.requireNonNull Object를 넘겨두고 만약에 Null이라면 예외를 던지고 아니면 이 값을 그대로 리턴해줌 (Null Check)
--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -1,30 +1,18 @@
 package com.practice;
 
-import java.io.IOException;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		
-		// Spring Container 생성  
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
-	 	
+		// Spring Container 생성
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+
 		// Spring Container 에 HelloController 라는 클래스를 활용하여 Bean 을 등록
 		// 파라미터를 주입받는 생성자가 있다면 해당 클래스도 Bean으로 등록해줘야 함
 		applicationContext.registerBean(HelloController.class);
@@ -36,51 +24,11 @@ public class PracticeApplication {
 		
 		// 내부 Tomcat 실행
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer( new ServletContextInitializer() {
-
-			@Override
-			public void onStartup(ServletContext servletContext) throws ServletException {
-				
-				servletContext.addServlet("FrontController", new HttpServlet() {
-
-					@Override
-					protected void service(HttpServletRequest req, HttpServletResponse resp)
-							throws ServletException, IOException {
-						
-						// 인증, 보안, 다국어, 공통 기능 (서비스 로직 전처리)
-						/* ... */
-						
-						// GET Method로 들어오는 /hello 요청에 대해서만 처리
-						if( req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name()) ) {
-						
-							String name = req.getParameter("name");
-							
-							// Spring Container에 등록한 HelloController 클래스로 지정된 Bean 을 참조하여 Object 를 가져옴
-							HelloController helloController = applicationContext.getBean(HelloController.class);
-							// Request에서 Parameter name을 꺼내오는 것은 요청을 받은 클래스에서 처리하도록 함
-							String ret = helloController.hello(name);
-
-							// 헤더의 Content-type 설정
-							resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-							// 바디 설정
-							resp.getWriter().println(ret);
-						
-						} 
-						else {
-							
-							// 의도하지 않은 요청으로 들어왔을 때 응답으로 BAD_REQUEST 리턴
-							resp.setStatus(HttpStatus.NOT_FOUND.value());
-							
-						}
-						
-					}
-					
-				}).addMapping("/*");
-				
-			}
-			
+		WebServer webServer = serverFactory.getWebServer( servletContext -> {
+				// 전체 요청에 대해서 Spring Container의 Bean 정보를 DispatcherServlet에 전달하여 맵핑이 이루어질 수 있도록 처리 
+				servletContext.addServlet( "dispatcherServlet", new DispatcherServlet(applicationContext) ).addMapping("/*");
 		});
-
+		
 		webServer.start();
 		
 	}
