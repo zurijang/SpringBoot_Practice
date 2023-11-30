c8cac71236821cb3fc3a97693eaef807b780ab67
zurijang <wkdwn05@gmail.com>
2023-11-30 11:12:44 +0900

    test : add test APITest, HelloController, HelloService

---
+++ src/test/java/com/practice/HelloServicetest.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloServicetest {
+
+	// 서버 동작을 시키지 않고 Java 단에서 테스트 수행 (테스트 수행 속도 빠름)
+	@Test
+	void simpleHelloService() {
+		
+		SimpleHelloService helloService = new SimpleHelloService();
+		
+		String ret = helloService.sayHello("Test");
+		
+		Assertions.assertThat(ret).isEqualTo("Hello Test");
+		
+	}
+	
+}
