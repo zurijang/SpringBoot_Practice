40ab3fdbe628642d0bd6537b737c0a192c9d5afa
zurijang <wkdwn05@gmail.com>
2023-12-01 10:42:22 +0900

    feat : add a star on the return String by using decorator pattern

--- src/main/java/com/practice/HelloController.java
+++ src/main/java/com/practice/HelloController.java
@@ -23,4 +23,5 @@ public class HelloController {
 		return helloService.sayHello(name);
 		
 	}
+	
 }
---
+++ src/main/java/com/practice/HelloDecorator.java
@@ -0,0 +1,25 @@
+package com.practice;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+// @Primary HelloService 구현체 가 2개 일 때 Autowiring 에 우선되도록 지정
+@Service
+@Primary
+public class HelloDecorator implements HelloService {
+
+	private final HelloService helloService;
+	
+	// HelloDecorator 기준 자기자신에 @Primary가 붙어 HelloService 오브젝트 타입의 구현체는 1개
+	public HelloDecorator(HelloService helloService) { 
+		this.helloService = helloService;
+	}
+	
+	@Override
+	public String sayHello(String name) {
+
+		return "*" + helloService.sayHello(name) + "*";
+	
+	}
+	
+}
