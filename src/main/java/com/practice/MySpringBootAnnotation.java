968ec78867387a24a499fb2a261b131ccdbf397e
zurijang <wkdwn05@gmail.com>
2023-12-01 14:35:16 +0900

    chore : create a custom springboot annocation

---
+++ src/main/java/com/practice/MySpringBootAnnotation.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@ComponentScan
+@Configuration
+public @interface MySpringBootAnnotation {
+
+}
--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -4,23 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class PracticeApplication {
-	
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
-	}
-	
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
 	
 	public static void main(String[] args) {
 		
