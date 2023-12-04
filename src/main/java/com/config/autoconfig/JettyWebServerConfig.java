53b9ee4f39cc03d9f9e9e824400c6fab3ed8fd47
zurijang <wkdwn05@gmail.com>
2023-12-04 17:42:33 +0900

    build : control to regist bean on container by using the @ConditionalOnMissingBean

--- src/main/java/com/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/config/autoconfig/JettyWebServerConfig.java
@@ -1,5 +1,6 @@
 package com.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,8 @@ public class JettyWebServerConfig {
 
 	// 기본적으로 Bean은 Method 명을 따라가지만 충돌이 날 경우 지정가능
 	@Bean("jettyWebServerFactory")
+	// Bean으로 등록된 Method의 반환타입과 동일한 Bean 정보가 없으면 등록 (Spring Boot Annotation)
+	@ConditionalOnMissingBean
 	public ServletWebServerFactory servletWebServerFactory() {
 		return new JettyServletWebServerFactory();
 	}
