e2effe8a86c00fb08e6b9e4cea76e2aae4d8b6e1
zurijang <wkdwn05@gmail.com>
2023-12-04 16:28:31 +0900

    refactor : abstract by using annotation element for checking library

--- src/main/java/com/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/config/autoconfig/JettyWebServerConfig.java
@@ -3,36 +3,19 @@ package com.config.autoconfig;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
+import com.config.ConditionalMyOnClass;
 import com.config.MyAutoConfiguration;
-import com.config.autoconfig.JettyWebServerConfig.JettyCondition;
 
 // MyAutoConfiguration 이라는 기능에 의해서 사용되는 대상이라는 것을 명시
 @MyAutoConfiguration
-@Conditional(JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
 
 	// 기본적으로 Bean은 Method 명을 따라가지만 충돌이 날 경우 지정가능
 	@Bean("jettyWebServerFactory")
 	public ServletWebServerFactory servletWebServerFactory() {
 		return new JettyServletWebServerFactory();
-	}
-	
-	static class JettyCondition implements Condition {
-
-		@Override
-		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-			
-			return ClassUtils.isPresent("org.eclipse.jetty.server.Server", 
-					context.getClassLoader());
-		
-		}
-		
 	}
 	
 }
--- src/main/java/com/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/config/autoconfig/TomcatWebServerConfig.java
@@ -3,36 +3,19 @@ package com.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
+import com.config.ConditionalMyOnClass;
 import com.config.MyAutoConfiguration;
-import com.config.autoconfig.TomcatWebServerConfig.TomcatCondition;
 
 // MyAutoConfiguration 이라는 기능에 의해서 사용되는 대상이라는 것을 명시
 @MyAutoConfiguration
-@Conditional(TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
 	// 기본적으로 Bean은 Method 명을 따라가지만 충돌이 날 경우 지정가능
 	@Bean("tomcatWebServerFactory")
 	public ServletWebServerFactory servletWebServerFactory() {
 		return new TomcatServletWebServerFactory();
-	}
-	
-	static class TomcatCondition implements Condition {
-
-		@Override
-		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-
-			return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", 
-					context.getClassLoader());
-
-		}
-		
 	}
 	
 }
