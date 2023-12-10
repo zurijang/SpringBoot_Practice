ec30b7f94399795730c907e92920a9a573f8b951
zurijang <wkdwn05@gmail.com>
2023-12-10 23:59:18 +0900

    refactor : create an annotation for combing annotations to test

--- src/test/java/com/practice/DataSourceTest.java
+++ src/test/java/com/practice/DataSourceTest.java
@@ -12,12 +12,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-// SpringContainer를 띄우고 빈 구성 정보를 넣고 해당 빈을 가져와서 테스트 진행
-@ExtendWith(SpringExtension.class)
-// 모든 빈 구성정보를 불러오는 클래스를 통해 Import와 ComponentScan 을 이용해서 자동 구성 빈, 애플리케이션 빈 이런 것들을 불러옴
-@ContextConfiguration(classes = PracticeApplication.class)
-// properties 파일을 읽어 test 진행 하도록 명시
-@TestPropertySource("classpath:/application.properties")
+@HellobootTest
 public class DataSourceTest {
 
 	@Autowired
