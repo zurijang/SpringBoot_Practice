3e1e626a2538ef5f763253672e1572bbaef617eb
zurijang <wkdwn05@gmail.com>
2023-12-11 22:50:55 +0900

    test : test a transaction about the hello object

---
+++ src/test/java/com/practice/HelloRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.practice;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+@HellobootTest
+@Transactional
+public class HelloRepositoryTest {
+
+	@Autowired JdbcTemplate jdbcTemplate;
+	@Autowired HelloRepository helloRepository;
+	
+	@BeforeEach
+	 void init() {
+		 
+		 jdbcTemplate.execute("create table if not exists member(name varchar(50) primary key, count int)");
+		 
+	 }
+	 
+	
+	@Test
+	void findHelloFailed() {
+		Assertions.assertThat(helloRepository.findHello("Zuri")).isNull();
+	}
+	
+	@Test
+	void increaseCount() {
+		Assertions.assertThat(helloRepository.countOf("Zuri")).isEqualTo(0);
+		
+		helloRepository.increaseCount("Zuri");
+		Assertions.assertThat(helloRepository.countOf("Zuri")).isEqualTo(1);
+		
+		helloRepository.increaseCount("Zuri");
+		Assertions.assertThat(helloRepository.countOf("Zuri")).isEqualTo(2);
+	}
+	
+}
