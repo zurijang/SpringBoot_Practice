14698ed836656f0f12e2f76543cf9b56ad6db221
zurijang <wkdwn05@gmail.com>
2023-12-11 22:49:49 +0900

    feat : add a simple function for the hello object repository

---
+++ src/main/java/com/practice/Hello.java
@@ -0,0 +1,31 @@
+package com.practice;
+
+public class Hello {
+
+	private String name;
+	private int count;
+	
+	public Hello(String name, int count) {
+		this.name = name;
+		this.count = count;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+	
+}
