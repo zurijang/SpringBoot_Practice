a5aa8984b603489463d491cdf79534a8625048c8
zurijang <wkdwn05@gmail.com>
2023-12-17 19:12:10 +0900

    build : connect MariaDB

--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -2,26 +2,9 @@ package com.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class PracticeApplication {
-	
-	private final JdbcTemplate jdbcTemplate;
-	
-	public PracticeApplication(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-	
-	// Spring Framework의 Lifecycle Interface를 이용한 방식을 간결하게 대체할 수 있는 용도로 사용
-	// 모든 프로퍼티 세팅까지 다 끝나면 수행 구현
-	@PostConstruct
-	void init() {
-		// Application 구동 시 인메모리 DB의 테이블 생성
-		jdbcTemplate.execute("create table if not exists member(name varchar(50) primary key, count int)");
-	}
 	
 	public static void main(String[] args) {
 		
